5d3acf00ac648f0552979bed11a2d885d172e393
Dave Wagner <dwagner650@gmail.com>
2019-06-11 17:07:22 -0400

    fixed title URL

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Daily Exchange Rate Converter | restapi.uncommoncode.com</title>
+	<title>Daily Exchange Rate Converter | rest-api.uncommoncode.com</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
